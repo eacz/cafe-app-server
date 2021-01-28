ffeca7004f21b2e618b0d97a154f8a145c1d1bc5
eacz <estebanacanteros@gmail.com>
2021-01-28 03:36:44 -0300

    config updated

--- server/config/config.js
+++ server/config/config.js
@@ -7,6 +7,6 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://127.0.0.1:27017/cafe'
 } else {
-    urlDB='mongodb+srv://eacz:cafeapp@main.aodkn.mongodb.net/cafe'
+    urlDB= process.env.MONGO_URI
 }
 process.env.MONGO_URL = urlDB;
